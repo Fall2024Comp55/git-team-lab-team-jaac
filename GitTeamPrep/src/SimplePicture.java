4b5c9eb5d1c6bed41f7c989dc5c97306671ce8a6
Yanjun1337 <y_deng1@u.pacific.edu>
2024-09-27 14:39:50 -0700

    efactor the String filename for our GImage as a constant。

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -4,13 +4,14 @@ import acm.program.GraphicsProgram;
 
 public class SimplePicture extends GraphicsProgram {
 	public static int SIZE = 700;
+	public static final String IMAGE_FILENAME = "robot.png";
 	
 	public void init() {
 		setSize(SIZE, SIZE);
 	}
 	
 	public void run() {
-		GImage robot = new GImage("robot.png", 200, 100);
+		GImage robot = new GImage(IMAGE_FILENAME, 200, 100);
 		add(robot);
 		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
 		add(label);
